340c806c09c1a478743562985b5ca85ebeb7a501
Jan Wawerek <17J.Wawerek@stmarys.net>
2023-01-09 12:56:27 +0000

    Task 1

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const getDateButton = document.getElementById("getDateButton");
 // output divs
 const timeOutput = document.getElementById("timeOutput");
 
+const getRandomButton = document.getElementById("getRandomButton");
+const randomOutput = document.getElementById("randomOutput");
+
 
 // link python functions to the buttons
 getDateButton.addEventListener("click", () => {
     timeOutput.innerText = app.getCurrentDateAndTime();
+});
+
+
+
+getRandomButton.addEventListener("click", () => {
+    randomOutput.innerText = app.getRandomNumber();
 });
